44cf4fe4b05d85ab2fac8d134960a405b5b46c8d
Etienne Bouron <etienne.toutbien@gmail.com>
2017-11-23 19:14:13 +0100

    basic with css

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,13 @@
+ 
+
+ $(function() {
+            $("#menu").tabs({
+               event:"mouseover"
+            });
+         });
+
+ $(document).ready( function () 
+ {
+    // On cache les sous-menus :
+    $(".navigation div.subMenu").hide();
+   
